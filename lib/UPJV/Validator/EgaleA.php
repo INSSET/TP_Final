abf6c91d1823e6cfd0ffc26f5c5bd6a4e1fc2e95
AlphaDeflo <erdeflo59@hotmail.com>
2024-04-29 15:53:45 +0200

    ensemble du egal

---
+++ lib/UPJV/Validator/EgaleA.php
@@ -0,0 +1,108 @@
+<?php
+
+/**
+ * PHP version 8.2.10
+ * Déclaration des savoirs faire pour chaque validator.
+ *
+ * @category Bibliotheque
+ *
+ * @package Validator
+ *
+ * @author Harold <harold.trannois@u-picardie.fr>
+ *
+ * @license gnu https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * @link https://pedag.u-picardie.fr/moodle/upjv/course/view.php?id=308#section-0
+ */
+
+namespace UPJV\Validator;
+
+/**
+ * Vérifie que la taille de la chaine comporte au moins x caractères
+ *  - x fixé par la méthode setMin
+ *  - x = 3 par défaut (3 caractères minimum)
+ */
+class EgaleA extends AbstractValidator
+{
+    /**
+     * Class Min implémente les méthode build & check
+     */
+    protected $E;
+    protected $data;
+    protected $flag;
+
+    /**
+     * Initialise les variables d'instance, pas de donnée, pas de résultat de validation
+     * Le nombre  par défaut 3
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->E = 3;
+        $this->flag = null;
+        $this->data = null;
+    }
+
+    /**
+     * Permet de fixer un autre nombre 
+     *
+     * @param int $min
+     */
+    public function setEgal($E)
+    {
+        $this->E = $E;
+    }
+
+    /**
+     * Test la longueur de la chaine de caractères
+     * et stock la donnée pris en charge dans data
+     *
+     * @param string $value la valeur à tester
+     *
+     * @return bool renvoi faux si le test ne passe pas
+     */
+    public function verifie($value)
+    {
+        $this->data = $value;
+       
+    
+        if ($value == $this->E) {
+            return $this->flag = true;
+        }
+        if ($value !==$this->E){
+            return $this->flag = false;
+            }
+            if($value == null){
+         return $this->flag = null;
+            }
+    }
+
+    /**
+     * Getter sur la donnée enregistrée
+     *
+     * @return null|string renvoi la donnée testée
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    /**
+     * En fonction de l'état du test renvoi rien | une erreur | la condition
+     *
+     * @return string|void
+     */
+    public function getMsgInfo()
+    {
+        if ($this->flag === false) {
+                
+            return "Le nombre est inférieur ou supérieur à " . $this->E;
+        }
+        if ($this->flag === true) {
+            return "C'est tout bon !";
+        }
+        if ($this->flag === null) {
+            return "un nombre égal à " . $this->E;
+        }
+    }
+}
---
+++ tests/UPJV/Validator/EgaleATest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace UPJV\Validator;
+
+require_once __DIR__ . '/../../../vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+use UPJV\Validator\EgaleA;
+
+class EgaleATest extends TestCase
+{
+    public function testConstructor()
+    {
+        $validator = new EgaleA();
+
+        $this->assertNull($validator->getData());
+    }
+
+    public function testDataGetterSetter()
+    {
+        $validator = new EgaleA();
+        $validator->verifie("test_data");
+
+        $this->assertEquals("test_data", $validator->getData());
+    }
+
+    public function testVerifie()
+    {
+        $validator = new EgaleA();
+
+        // Test avec un nombre inférieur  minimale
+        $this->assertFalse($validator->verifie(2));
+
+        // Test avec nombre minimal
+        $this->assertTrue($validator->verifie(3));
+
+        // Test avec un nombre supérieur minimale
+        $this->assertTrue($validator->verifie(5));
+    }
+
+    public function testGetMsgInfo()
+    {
+        $validator = new MinLong();
+
+        // Test lorsque le test n'a pas encore été exécuté
+        $this->assertEquals("un nombre égal à 3", $validator->getMsgInfo());
+
+        // Test lorsque le test échoue
+        $validator->verifie(2);
+        $this->assertEquals("Le nombre est inférieur ou supérieur à 3", $validator->getMsgInfo());
+
+        // Test lorsque le test réussit
+        $validator->verifie(3);
+        $this->assertEquals("C'est tout bon !", $validator->getMsgInfo());
+    }
+}
