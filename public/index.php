2a668290301ff07490bc5f15895edfc6f97b7d92
MEMMI-Abdelmouez <abdelmouez.memmi@gmail.com>
2024-04-29 16:41:22 +0200

    commit tous 6

--- public/index.php
+++ public/index.php
@@ -12,9 +12,9 @@
 <div class="p-3 list-group text-center">
     <a href="#" class="list-group-item list-group-item-action active" aria-current="true"><b>LISTES DES EXEMPLES DE VALIDATEUR</b></a>
     <a href="forms/min_long.php" class="list-group-item list-group-item-action">Exemple Validateur de la longueur minimale d'une chaîne de caractère <i>(Harold Trannois)</i></a>
-    <a href="forms/Appartient_A.php" class="list-group-item list-group-item-action">AppartientA (RegEx) <i>(Abdelmouez MEMMI)</i></a>
     <a href="forms/superieur_a.php" class="list-group-item list-group-item-action">Exemple Validateur pour vérifier qu'une valeur est supérieur à une autre valeur <i>(Melvin Pollier)</i></a>
     <a href="forms/avant_date.php" class="list-group-item list-group-item-action">Exemple Validateur d'une date antérieur <i>(Théo RAYMOND)</i></a>
+    <a href="forms/Appartient_A.php" class="list-group-item list-group-item-action">AppartientA (RegEx) <i>(Abdelmouez MEMMI)</i></a>
 </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
